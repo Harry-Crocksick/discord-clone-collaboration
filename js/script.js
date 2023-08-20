4c4da6dadd27010028e0e72c5385629d334ca88a
Harry Crocksick <harrycrocksick1inamillion@gmail.com>
2023-08-21 01:13:14 +0630

    Modified & updated primary nav & hamburger nav

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 const buttons = document.querySelectorAll(".depends-os>span");
-
 let OSName = "";
 
 if (window.navigator.userAgent.indexOf("Windows") != -1) OSName = "Windows";
@@ -9,3 +8,23 @@ if (window.navigator.userAgent.indexOf("Linux") != -1) OSName = "Linux";
 for (const button of buttons) {
   button.innerText = `Download for ${OSName}`;
 }
+
+const closeBtn = document.querySelector(".closeBtn");
+const hamburgerNav = document.querySelector(".hamburger-navbar");
+
+closeBtn.addEventListener("click", () => {
+  hamburgerNav.style.transform = "translateX(100%)";
+});
+
+const hamburgerBtn = document.querySelector(".hamburger-btn");
+
+hamburgerBtn.addEventListener("click", () => {
+  hamburgerNav.style.transform = "translateX(0)";
+});
+
+const mediaQuery = window.matchMedia("(min-width: 1024px)");
+mediaQuery.addEventListener("change", (e) => {
+  if (e.matches) {
+    hamburgerNav.style.transform = "translateX(100%)";
+  }
+});
