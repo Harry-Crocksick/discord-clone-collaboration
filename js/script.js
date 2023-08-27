1917cf92cb8c4910dd41438c1c06586c2862a9ac
Harry-Crocksick <141847189+Harry-Crocksick@users.noreply.github.com>
2023-08-28 03:05:39 +0630

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@ const buttons = document.querySelectorAll(".depends-os>span");
 let OSName = "";
 
 if (window.navigator.userAgent.indexOf("Windows") != -1) OSName = "Windows";
-if (window.navigator.userAgent.indexOf("Mac") != -1) OSName = "Mac";
-if (window.navigator.userAgent.indexOf("Linux") != -1) OSName = "Linux";
+else if (window.navigator.userAgent.indexOf("Mac") != -1) OSName = "Mac";
+else if (window.navigator.userAgent.indexOf("Linux") != -1) OSName = "Linux";
+else OSName = "Mobile";
 
 for (const button of buttons) {
   button.innerText = `Download for ${OSName}`;
